685deaeb4f4fe77b0970e7f33804f7e30f96b40c
Wellington <wellingtonjoaquim736@gmail.com>
2025-08-30 21:23:03 -0300

    Estrutura do Projeto

---
+++ TCC/src/test/java/projeto_amor_e_acao/TCC/TccApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto_amor_e_acao.TCC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
