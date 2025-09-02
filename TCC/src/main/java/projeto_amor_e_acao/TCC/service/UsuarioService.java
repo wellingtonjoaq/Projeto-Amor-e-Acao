c29581ace0fa27818d5e0e1ad149058933701f91
Igor <igorantonucci5@gmail.com>
2025-09-02 20:40:07 -0300

    Receba!

---
+++ TCC/src/main/java/projeto_amor_e_acao/TCC/service/UsuarioService.java
@@ -0,0 +1,32 @@
+package projeto_amor_e_acao.TCC.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import projeto_amor_e_acao.TCC.model.Usuario;
+import projeto_amor_e_acao.TCC.repository.UsuarioRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public List<Usuario> findAll() {
+        return usuarioRepository.findAll();
+    }
+
+    public Optional<Usuario> findById(Long id) {
+        return usuarioRepository.findById(id);
+    }
+
+    public Usuario save(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    public void deleteById(Long id) {
+        usuarioRepository.deleteById(id);
+    }
+}
