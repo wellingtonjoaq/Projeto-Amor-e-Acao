48b135df0d3e5713882173182876090d8f1bc3cc
Igor <igorantonucci5@gmail.com>
2025-09-19 22:01:43 -0300

    Tratamento de excessões em empresas parceiras

--- TCC/src/main/java/projeto_amor_e_acao/TCC/repository/EmpresaParceiraRepository.java
+++ TCC/src/main/java/projeto_amor_e_acao/TCC/repository/EmpresaParceiraRepository.java
@@ -1,7 +1,12 @@
 package projeto_amor_e_acao.TCC.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import projeto_amor_e_acao.TCC.model.EmpresaParceira;
 
+import java.util.Optional;
+
+@Repository
 public interface EmpresaParceiraRepository extends JpaRepository<EmpresaParceira, Long> {
+    Optional<EmpresaParceira> findByCnpj(String cnpj);
 }
