3b8c1e31e4f77d244445b3c76ff8ebf4c039973f
SamuelAlves903 <alvessamuel205@gmail.com>
2025-11-02 20:30:18 -0300

    Crição Filtros (Aluno, Voluntario e Curso)

--- TCC/src/main/java/projeto_amor_e_acao/TCC/repository/AlunoRepository.java
+++ TCC/src/main/java/projeto_amor_e_acao/TCC/repository/AlunoRepository.java
@@ -4,10 +4,34 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import projeto_amor_e_acao.TCC.model.Aluno;
+import projeto_amor_e_acao.TCC.model.Curso;
+import projeto_amor_e_acao.TCC.model.Voluntario;
 
 import java.util.List;
 
 public interface AlunoRepository extends JpaRepository<Aluno, Long> {
-    Page<Aluno> findByStatusIgnoreCase(String status, Pageable pageable);
+
     List<Aluno> findByStatusIgnoreCase(String status);
+
+    Page<Aluno> findByStatusIgnoreCase(String status, Pageable pageable);
+
+    Page<Aluno> findByStatusIgnoreCaseAndGeneroIgnoreCase(String status, String genero, Pageable pageable);
+
+    Page<Aluno> findByStatusIgnoreCaseAndMatriculas_CursoIn(String status, List<Curso> cursos, Pageable pageable);
+
+    Page<Aluno> findByStatusIgnoreCaseAndMatriculas_CursoInAndGeneroIgnoreCase(String status, List<Curso> cursos, String genero, Pageable pageable);
+
+    Page<Aluno> findByStatusIgnoreCaseAndNomeContainingIgnoreCase(String status, String nome, Pageable pageable);
+
+    Page<Aluno> findByStatusIgnoreCaseAndCpfContainingIgnoreCase(String status, String cpf, Pageable pageable);
+
+    Page<Aluno> findByStatusIgnoreCaseAndEmailContainingIgnoreCase(String status, String email, Pageable pageable);
+
+    Page<Aluno> findByStatusIgnoreCaseAndTelefoneContainingIgnoreCase(String status, String telefone, Pageable pageable);
+
+    Page<Aluno> findByStatusIgnoreCaseAndCepContainingIgnoreCase(String status, String cep, Pageable pageable);
+
+    Page<Aluno> findByStatusIgnoreCaseAndEnderecoContainingIgnoreCase(String status, String endereco, Pageable pageable);
+
+    Page<Aluno> findByStatusIgnoreCaseAndBairroContainingIgnoreCase(String status, String bairro, Pageable pageable);
 }
--- TCC/src/main/java/projeto_amor_e_acao/TCC/repository/VoluntarioRepository.java
+++ TCC/src/main/java/projeto_amor_e_acao/TCC/repository/VoluntarioRepository.java
@@ -4,12 +4,39 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import projeto_amor_e_acao.TCC.model.Aluno;
-import projeto_amor_e_acao.TCC.model.EmpresaParceira;
+import projeto_amor_e_acao.TCC.model.Curso;
+import projeto_amor_e_acao.TCC.model.FuncaoVoluntario;
 import projeto_amor_e_acao.TCC.model.Voluntario;
 
 import java.util.List;
 
 public interface VoluntarioRepository extends JpaRepository<Voluntario, Long> {
-    Page<Voluntario> findByStatusIgnoreCase(String status, Pageable pageable);
     List<Voluntario> findByStatusIgnoreCase(String status);
+
+    Page<Voluntario> findByStatusIgnoreCase(String status, Pageable pageable);
+
+    Page<Voluntario> findByStatusIgnoreCaseAndGeneroIgnoreCase(String status, String genero, Pageable pageable);
+
+    Page<Voluntario> findByStatusIgnoreCaseAndFuncaoIn(String status, List<FuncaoVoluntario> funcao, Pageable pageable);
+
+    Page<Voluntario> findByStatusIgnoreCaseAndFuncaoInAndGeneroIgnoreCase(String status, List<FuncaoVoluntario> funcao, String genero, Pageable pageable);
+
+    Page<Voluntario> findByStatusIgnoreCaseAndNomeContainingIgnoreCase(String status, String nome, Pageable pageable);
+
+    Page<Voluntario> findByStatusIgnoreCaseAndCpfContainingIgnoreCase(String status, String cpf, Pageable pageable);
+
+    Page<Voluntario> findByStatusIgnoreCaseAndEmailContainingIgnoreCase(String status, String email, Pageable pageable);
+
+    Page<Voluntario> findByStatusIgnoreCaseAndTelefoneContainingIgnoreCase(String status, String telefone, Pageable pageable);
+
+    Page<Voluntario> findByStatusIgnoreCaseAndCepContainingIgnoreCase(String status, String cep, Pageable pageable);
+
+    Page<Voluntario> findByStatusIgnoreCaseAndEnderecoContainingIgnoreCase(String status, String endereco, Pageable pageable);
+
+    Page<Voluntario> findByStatusIgnoreCaseAndBairroContainingIgnoreCase(String status, String bairro, Pageable pageable);
+
+    Page<Voluntario> findByStatusIgnoreCaseAndCidadeContainingIgnoreCase(String status, String cidade, Pageable pageable);
+
+    Page<Voluntario> findByStatusIgnoreCaseAndEstadoContainingIgnoreCase(String status, String estado, Pageable pageable);
+
 }
