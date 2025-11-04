ab5b0e669973dd91f6e109df97b61ade2bcf6fd6
Igor <igorantonucci5@gmail.com>
2025-11-04 19:06:58 -0300

    Resolução de conflitos.

--- TCC/src/main/java/projeto_amor_e_acao/TCC/repository/AlunoRepository.java
+++ TCC/src/main/java/projeto_amor_e_acao/TCC/repository/AlunoRepository.java
@@ -1,9 +1,5 @@
 package projeto_amor_e_acao.TCC.repository;
 
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import projeto_amor_e_acao.TCC.model.EmpresaParceira;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
