d2ceb246428ae2e6e7738838664835a919260840
Igor <igorantonucci5@gmail.com>
2025-11-04 19:02:56 -0300

    Resolução de conflitos.

--- TCC/src/main/java/projeto_amor_e_acao/TCC/repository/AlunoRepository.java
+++ TCC/src/main/java/projeto_amor_e_acao/TCC/repository/AlunoRepository.java
@@ -14,48 +14,27 @@ import projeto_amor_e_acao.TCC.model.Voluntario;
 import java.util.List;
 
 public interface AlunoRepository extends JpaRepository<Aluno, Long> {
-    Long countByStatus(String status);
-
-    @Query("SELECT COUNT(a) FROM Aluno a JOIN a.matriculas m WHERE m.curso.id = :cursoId")
-    Long countAlunosMatriculadosByCursoId(@Param("cursoId") Long cursoId);
-
-    @Query("SELECT COUNT(a) FROM Aluno a JOIN a.matriculas m WHERE m.curso.id = " +
-            ":cursoId AND a.status = :status")
-    Long countAlunosEvasivosByCursoIdAndStatus(
-            @Param("cursoId") Long cursoId, @Param("status") String status);
-
     List<Aluno> findByStatusIgnoreCase(String status);
 
-    Page<Aluno> findByStatusIgnoreCase(
-            String status, Pageable pageable);
+    Page<Aluno> findByStatusIgnoreCase(String status, Pageable pageable);
 
-    Page<Aluno> findByStatusIgnoreCaseAndGeneroIgnoreCase(
-            String status, String genero, Pageable pageable);
+    Page<Aluno> findByStatusIgnoreCaseAndGeneroIgnoreCase(String status, String genero, Pageable pageable);
 
-    Page<Aluno> findByStatusIgnoreCaseAndMatriculas_CursoIn(
-            String status, List<Curso> cursos, Pageable pageable);
+    Page<Aluno> findByStatusIgnoreCaseAndMatriculas_CursoIn(String status, List<Curso> cursos, Pageable pageable);
 
-    Page<Aluno> findByStatusIgnoreCaseAndMatriculas_CursoInAndGeneroIgnoreCase(
-            String status, List<Curso> cursos, String genero, Pageable pageable);
+    Page<Aluno> findByStatusIgnoreCaseAndMatriculas_CursoInAndGeneroIgnoreCase(String status, List<Curso> cursos, String genero, Pageable pageable);
 
-    Page<Aluno> findByStatusIgnoreCaseAndNomeContainingIgnoreCase(
-            String status, String nome, Pageable pageable);
+    Page<Aluno> findByStatusIgnoreCaseAndNomeContainingIgnoreCase(String status, String nome, Pageable pageable);
 
-    Page<Aluno> findByStatusIgnoreCaseAndCpfContainingIgnoreCase(
-            String status, String cpf, Pageable pageable);
+    Page<Aluno> findByStatusIgnoreCaseAndCpfContainingIgnoreCase(String status, String cpf, Pageable pageable);
 
-    Page<Aluno> findByStatusIgnoreCaseAndEmailContainingIgnoreCase(
-            String status, String email, Pageable pageable);
+    Page<Aluno> findByStatusIgnoreCaseAndEmailContainingIgnoreCase(String status, String email, Pageable pageable);
 
-    Page<Aluno> findByStatusIgnoreCaseAndTelefoneContainingIgnoreCase(
-            String status, String telefone, Pageable pageable);
+    Page<Aluno> findByStatusIgnoreCaseAndTelefoneContainingIgnoreCase(String status, String telefone, Pageable pageable);
 
-    Page<Aluno> findByStatusIgnoreCaseAndCepContainingIgnoreCase(
-            String status, String cep, Pageable pageable);
+    Page<Aluno> findByStatusIgnoreCaseAndCepContainingIgnoreCase(String status, String cep, Pageable pageable);
 
-    Page<Aluno> findByStatusIgnoreCaseAndEnderecoContainingIgnoreCase(
-            String status, String endereco, Pageable pageable);
+    Page<Aluno> findByStatusIgnoreCaseAndEnderecoContainingIgnoreCase(String status, String endereco, Pageable pageable);
 
-    Page<Aluno> findByStatusIgnoreCaseAndBairroContainingIgnoreCase(
-            String status, String bairro, Pageable pageable);
+    Page<Aluno> findByStatusIgnoreCaseAndBairroContainingIgnoreCase(String status, String bairro, Pageable pageable);
 }
