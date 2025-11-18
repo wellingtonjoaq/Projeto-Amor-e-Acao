3197dbf6c061104ad0739e9b43a7fc76fc05e978
Igor <igorantonucci5@gmail.com>
2025-11-18 20:56:09 -0300

    Implementação e estilização das telas de erro, e dos filtros de relatórios.

--- TCC/src/main/java/projeto_amor_e_acao/TCC/repository/LogAuditoriaRepository.java
+++ TCC/src/main/java/projeto_amor_e_acao/TCC/repository/LogAuditoriaRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import projeto_amor_e_acao.TCC.model.LogAuditoria;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.time.LocalDateTime;
 
 public interface LogAuditoriaRepository extends JpaRepository<LogAuditoria, Long> {
 
@@ -14,4 +16,10 @@ public interface LogAuditoriaRepository extends JpaRepository<LogAuditoria, Long
 
     @Query("SELECT COUNT(l) FROM LogAuditoria l WHERE l.acao IN ('CREATE', 'UPDATE', 'DELETE')")
     Long countTotalAcoesChave();
+
+    @Query("SELECT COUNT(l) FROM LogAuditoria l WHERE l.dataHora BETWEEN :dataInicio AND :dataFim")
+    long countTotalAcoesChaveBetweenDates(LocalDateTime dataInicio, LocalDateTime dataFim);
+
+    @Query("SELECT CONCAT(l.usuarioId, ' - ', l.acao) AS key, COUNT(l) FROM LogAuditoria l WHERE l.dataHora BETWEEN :dataInicio AND :dataFim GROUP BY l.usuarioId, l.acao")
+    List<Object[]> countAcoesAgrupadasPorUsuarioEAcaoBetweenDates(LocalDateTime dataInicio, LocalDateTime dataFim);
 }
