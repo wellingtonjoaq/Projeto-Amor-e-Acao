2d5e79634031264c1a5d7b97c5801b96a783a84a
SamuelAlves903 <alvessamuel205@gmail.com>
2025-11-18 21:19:59 -0300

    Criação Controller Visitantes e Ajuste de rotas em visitantes

---
+++ TCC/src/main/java/projeto_amor_e_acao/TCC/controller/VisitantesController.java
@@ -0,0 +1,35 @@
+package projeto_amor_e_acao.TCC.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("visitantes")
+public class VisitantesController {
+
+    @GetMapping("/")
+    public String index() {
+        return "visitantes/index";
+    }
+
+    @GetMapping("cursos")
+    public String curso() {
+        return "visitantes/curso/lista";
+    }
+
+    @GetMapping("doacoes")
+    public String doacoes() {
+        return "visitantes/doacoes/lista";
+    }
+
+    @GetMapping("sobre")
+    public String sobre() {
+        return "visitantes/sobreNos/lista";
+    }
+
+    @GetMapping("login")
+    public String login() {
+        return "redirect:/login";
+    }
+}
