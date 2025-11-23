e82d08daae838632b82cd15fac2f072e718c260c
SamuelAlves903 <alvessamuel205@gmail.com>
2025-11-22 22:31:19 -0300

    Pagina de Cursos e Matricula Funcional e alguns ajustes no projeto

--- TCC/src/main/java/projeto_amor_e_acao/TCC/controller/VisitantesController.java
+++ TCC/src/main/java/projeto_amor_e_acao/TCC/controller/VisitantesController.java
@@ -2,20 +2,17 @@ package projeto_amor_e_acao.TCC.controller;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import projeto_amor_e_acao.TCC.model.EmpresaParceira;
-import projeto_amor_e_acao.TCC.model.Usuario;
-import projeto_amor_e_acao.TCC.model.Voluntario;
-import projeto_amor_e_acao.TCC.service.EmpresaParceiraService;
-import projeto_amor_e_acao.TCC.service.FuncaoVoluntarioService;
-import projeto_amor_e_acao.TCC.service.VoluntarioService;
+import projeto_amor_e_acao.TCC.model.*;
+import projeto_amor_e_acao.TCC.service.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("visitantes")
@@ -30,14 +27,95 @@ public class VisitantesController {
     @Autowired
     private EmpresaParceiraService empresaParceiraService;
 
+    @Autowired
+    private CursoService cursoService;
+
+    @Autowired
+    private AlunoService alunoService;
+
     @GetMapping("/")
     public String index() {
         return "visitantes/index";
     }
 
     @GetMapping("cursos")
-    public String curso() {
+    public String curso(@RequestParam(defaultValue = "0") int page,
+                        @RequestParam(defaultValue = "12") int size,
+                        Model model) {
+
+        Page<Curso> cursos = cursoService.listarPaginados(page, size);
+
+        model.addAttribute("cursos", cursos);
+        model.addAttribute("paginaAtual", page);
         return "visitantes/curso/lista";
+    }
+
+    @GetMapping("cursos/verTodos")
+    public String verTodos(@RequestParam(defaultValue = "0") int page,
+                        @RequestParam(defaultValue = "12") int size,
+                        Model model) {
+
+        Page<Curso> cursos = cursoService.listarPaginados(page, size);
+
+        model.addAttribute("cursos", cursos);
+        model.addAttribute("paginaAtual", page);
+        return "visitantes/curso/verTodos/lista";
+    }
+
+    @GetMapping("cursos/filtrarPesquisa")
+    public String filtrarPesquisa(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "12") int size,
+            @RequestParam(required = false) String pesquisa,
+            Model model) {
+
+        if (!pesquisa.isEmpty()){
+            Page<Curso> cursos = cursoService.filtrarPesquisa(pesquisa, page, size);
+
+            model.addAttribute("pesquisa", pesquisa);
+            model.addAttribute("cursos", cursos);
+            model.addAttribute("paginaAtual", page);
+            model.addAttribute("vazio", cursos.isEmpty());
+
+            return "visitantes/curso/pesquisaFiltro/lista";
+        }
+        else {
+            return "redirect:/visitantes/cursos/verTodos";
+        }
+    }
+
+    @GetMapping("cursos/filtrar")
+    public String filtrar(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "12") int size,
+            @RequestParam(required = false) String status,
+            @RequestParam(required = false) String categoria,
+            @RequestParam(required = false) String periodo,
+            Model model) {
+
+        boolean temCategoria = (categoria != null && !categoria.isEmpty());
+        boolean temPeriodo = (periodo != null && !periodo.isEmpty());
+        boolean temStatus = (status != null && !status.isEmpty());
+
+        Page<Curso> cursos = cursoService.filtrar(categoria, periodo, status, page, size);
+
+        model.addAttribute("cursos", cursos);
+        model.addAttribute("paginaAtual", page);
+        model.addAttribute("categoria", categoria);
+        model.addAttribute("status", status);
+        model.addAttribute("periodo", periodo);
+        model.addAttribute("vazio", false);
+
+        if (!temCategoria && !temStatus && !temPeriodo) {
+            return "redirect:/visitantes/curso/verTodos";
+        }
+
+        if (cursos.isEmpty()){
+            model.addAttribute("vazio", cursos.isEmpty());
+        }
+
+
+        return "visitantes/curso/filtro/lista";
     }
 
     @GetMapping("doacoes")
@@ -81,9 +159,64 @@ public class VisitantesController {
     }
 
     @GetMapping("matricula")
-    public String matricula() {
+    public String matricula(Aluno aluno, Model model) {
+        model.addAttribute("cursos", cursoService.listarTodos());
         return "visitantes/matricula/formulario";
     }
+
+    @PostMapping("matricula/salvar")
+    public String salvar(
+            @Valid Aluno aluno,
+            BindingResult result,
+            @RequestParam(value = "cursosSelecionados", required = false) List<Long> cursosIds,
+            RedirectAttributes redirectAttributes,
+            Model model) {
+
+        if (result.hasErrors()) {
+            model.addAttribute("cursos", cursoService.listarTodos());
+            model.addAttribute("aluno", aluno);
+            return "visitantes/matricula/formulario";
+        }
+
+        if (cursosIds == null || cursosIds.isEmpty()) {
+            model.addAttribute("cursos", cursoService.listarTodos());
+            model.addAttribute("aluno", aluno);
+            model.addAttribute("matriculaErro", "( Selecione pelo menos um curso! )");
+            return "visitantes/matricula/formulario";
+        }
+
+        try {
+            cursosIds.forEach(cursoId -> {
+                Curso curso = cursoService.buscarPorId(cursoId);
+                Matricula matricula = new Matricula();
+                matricula.setCurso(curso);
+                matricula.setAluno(aluno);
+                aluno.getMatriculas().add(matricula);
+            });
+
+            aluno.setStatus("PENDENT");
+
+            alunoService.salvar(aluno);
+            return "redirect:/visitantes/cursos";
+        }
+        catch (IllegalStateException e) {
+            if (e.getMessage().contains("CPF")) {
+                result.rejectValue("cpf", "error.aluno", e.getMessage());
+            } else if (e.getMessage().contains("E-mail")) {
+                result.rejectValue("email", "error.aluno", e.getMessage());
+            }
+
+            model.addAttribute("cursos", cursoService.listarTodos());
+            return "visitantes/matricula/formulario";
+        } catch (Exception e) {
+
+            model.addAttribute("erro", e.getMessage());
+            model.addAttribute("aluno", aluno);
+            model.addAttribute("cursos", cursoService.listarTodos());
+            return "visitantes/matricula/formulario";
+        }
+    }
+
 
     @GetMapping("parceria")
     public String parceria(Model model) {
